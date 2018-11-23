8aba7e638471a951029bbcd76f7ccaee41881029
857610142 <857610142@qq.com>
2018-11-23 12:41:25 +0800

    Update rgb2y(ycrcb).m

--- rgb2y(ycrcb).m
+++ rgb2y(ycrcb).m
@@ -5,5 +5,6 @@ for i=1:num
     image = imread(pics(i).name)
     g = rgb2ycbcr(image)
     g = im2double(g(:, :, 1));
+    %%imwrite(g,['train91_gray\',strcat(pics(i).name,'.png')]);
     imwrite(g,['new_dir_name\',pics(i).name]);
 end
