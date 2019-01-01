1f868b3a5120fc201dfc6f3c1f2fff482d53565e
857610142 <857610142@qq.com>
2019-01-01 20:47:04 +0800

    Update crop.m

--- crop.m
+++ crop.m
@@ -1,13 +1,13 @@
 pics = dir('*.png')
 num = length(pics)
 cropnum = 500
-cropsize = 31
+cropsize = 32
 cropstart = 0
 stride = 6
 finnalpixel = stride*cropnum+1
 
 for i=1:num
-    image = imread(strcat(num2str(i),'.png'))
+    image = imread(pics(i).name)
     r = image(:,:,1)
     [n m] = size(r)
     new = 1
@@ -19,8 +19,8 @@ for i=1:num
                 if (((m-cropstart-k)<cropsize)) == 1   %小于裁剪大小便跳至下一张图片
                     break
                 else    
-                    g = imcrop(image,[cropstart+k cropstart+j cropsize cropsize])
-                    imwrite(g,['2\',strcat(num2str(i),strcat('-',strcat(num2str(new),'.png')))]);
+                    g = imcrop(image,[cropstart+j cropstart+k cropsize-1 cropsize-1])
+                    imwrite(g,['2\',strcat(pics(i).name(1:end-4),strcat('-',strcat(num2str(new),'.png')))]);
                     new = new+1
                 end       
             end
